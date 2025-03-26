b5578be6a4cf850f5ea5949277f3c7a6748f9565
jitendra kumar <128616359+jittupal@users.noreply.github.com>
2025-03-26 10:52:17 +0530

    Update Chat.jsx

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -242,12 +242,12 @@ export default function Chat() {
             <div key={message._id} className={(message.sender === id ? 'text-right' : 'text-left')}>
               <div className={"text-left inline-block p-3 my-2 rounded-xl text-sm shadow-md " +(message.sender === id ? 'bg-green-500 text-white' : 'bg-white text-gray-800 border border-gray-300')}>
                 {message.text}
-                <button
+{/*                 <button
                   onClick={() => deleteMessage(message._id)}
                   className="ml-2 text-red-500 text-sm hover:text-red-700 transition"
                 >
                   Delete
-                </button>
+                </button> */}
                 {message.file && (
                   <div>
                     <a target="_blank" className="flex items-center gap-1 border-b text-black hover:text-green-600 transition" href={axios.defaults.baseURL + '/uploads/' + message.file}>
@@ -282,12 +282,12 @@ export default function Chat() {
           onChange={ev => { setNewMessageText(ev.target.value); handleTyping(); }}
           placeholder="Type your message here"
           className="bg-gray-100 flex-grow border border-gray-300 rounded-lg p-3 shadow-sm focus:ring-2 focus:ring-green-300"/>
-      <label className="bg-green-400 p-3 text-white cursor-pointer rounded-lg border border-green-500 hover:bg-green-500 transition-all">
+{/*       <label className="bg-green-400 p-3 text-white cursor-pointer rounded-lg border border-green-500 hover:bg-green-500 transition-all">
         <input type="file" className="hidden" onChange={sendFile} />
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
           <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
         </svg>
-      </label>
+      </label> */}
       <button type="submit" className="bg-green-500 p-3 text-white rounded-lg shadow-md hover:bg-green-600 transition-all">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
